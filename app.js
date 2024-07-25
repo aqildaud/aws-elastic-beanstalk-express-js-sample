c4d2488d27b00c38d3760502d726f26acd4e7a1f
Aqil Mat Daud <aqildaud611@gmail.com>
2024-07-25 16:25:09 +0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Do not talk about my mom yo!'));
+app.get('/', (req, res) => res.send('There is only one beer left'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
